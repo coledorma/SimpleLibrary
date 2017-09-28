2ec88b4f044b1792b8c3cf8e0ee522926b90d3b6
Cole Dorma <coledorma@hotmail.com>
2017-09-28 13:12:18 -0400

    Testing setters.

--- src/com/tests/ItemTests.java
+++ src/com/tests/ItemTests.java
@@ -9,16 +9,27 @@ import server.logic.model.Item;
 
 public class ItemTests {
 	Item testItem = null;
+	Item testItem2 = null;
 	
 	@Before
 	public void setup() {
-		testItem = new Item(1, "3N6892F");	
+		testItem = new Item(1, "3N6892F");
+		
+		testItem2 = new Item(5, "555555H");
+		testItem2.setISBN("333333F");
+		testItem2.setItemid(7);
 	}
 	
 	@Test
 	public void testGetters() {
 		assertEquals(1, testItem.getItemid());
 		assertEquals("3N6892F", testItem.getISBN());
+	}
+	
+	@Test
+	public void testSetters() {
+		assertEquals(7, testItem2.getItemid());
+		assertEquals("333333F", testItem2.getISBN());
 	}
 
 }
