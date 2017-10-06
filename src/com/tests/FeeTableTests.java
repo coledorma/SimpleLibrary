c336da12ba2a3b535f62b66127ae88b29f9e629d
Cole Dorma <coledorma@hotmail.com>
2017-10-06 11:24:32 -0400

    Test and implemented FeeTable, getFeeTable and applyFee as these were all needed to create the feeTable object and the FeeTable object was all needed by the LoanTable class

---
+++ src/com/tests/FeeTableTests.java
@@ -0,0 +1,56 @@
+package com.tests;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.logic.model.Fee;
+import server.logic.tables.FeeTable;
+
+public class FeeTableTests {
+	FeeTable feeTable = null;
+	
+	@Before
+	public void setup() {
+		feeTable = FeeTable.getInstance();
+	}
+	
+	@Test
+	public void testGetFeeTable() {	
+		List<Fee> feeList = feeTable.getFeeTable();
+		
+		assertEquals(0, feeList.get(0).getUserid());
+		
+		assertEquals(1, feeList.get(1).getUserid());
+		assertEquals(100, feeList.get(1).getFee());
+		
+		assertEquals(2, feeList.get(2).getUserid());
+		
+	}
+	
+	@Test
+	public void testApplyFee() {
+		List<Fee> feeList = feeTable.getFeeTable();
+		
+		feeTable.applyfee(3, 360000);
+		assertEquals(1, feeList.get(3).getFee());
+		assertEquals(3, feeList.get(3).getUserid());
+		
+		feeTable.applyfee(4, 2000);
+		assertEquals(0, feeList.get(4).getFee());
+		assertEquals(4, feeList.get(4).getUserid());
+		
+		feeTable.applyfee(0, 360000);
+		assertEquals(6, feeList.get(0).getFee());
+		assertEquals(0, feeList.get(0).getUserid());
+		
+		feeTable.applyfee(1, 50000);
+		assertEquals(100, feeList.get(1).getFee());
+		assertEquals(1, feeList.get(1).getUserid());
+		
+	}
+	
+}
---
+++ src/server/logic/tables/FeeTable.java
@@ -0,0 +1,78 @@
+package server.logic.tables;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import server.logic.model.Fee;
+import server.logic.model.Loan;
+import utilities.Config;
+
+
+public class FeeTable {
+	List<Fee> feeList=new ArrayList<Fee>();
+	
+    private static class FeeListHolder {
+        private static final FeeTable INSTANCE = new FeeTable();
+    }
+    
+    private FeeTable(){
+    		//set up the default list with some instances
+    		Fee fee = new Fee(0,5);
+    		Fee fee2 = new Fee(1,100);
+    		Fee fee3 = new Fee(2,3);
+    		feeList.add(fee);
+    		feeList.add(fee2);
+    		feeList.add(fee3);
+    		Initialization();
+    };
+    
+    public static final FeeTable getInstance() {
+        return FeeListHolder.INSTANCE;
+    }
+    
+    public List<Fee> getFeeTable() {
+		return feeList;
+	}
+	public void Initialization(){
+    		List<Loan> loanList=LoanTable.getInstance().getLoanTable();
+    		for(int i=0;i<loanList.size();i++){
+    			applyfee(loanList.get(i).getUserid(), new Date().getTime()-loanList.get(i).getDate().getTime());
+    		}
+	}
+	
+	public void applyfee(int j, long time) {
+		int flag=0;
+		int index=0;
+		for(int i = 0;i<feeList.size();i++){
+			int userid=(feeList.get(i)).getUserid();
+			if(userid==j){
+				flag=flag+1;
+				index=i;
+			}
+		}
+		
+		int a=(int) ((time/(Config.STIMULATED_DAY))-Config.OVERDUE);
+		if(flag!=0){
+			if(a>=0){
+				feeList.get(index).setFee(a+feeList.get(index).getFee());
+				feeList.get(index).setUserid(j);
+			}else{
+				feeList.get(index).setFee(feeList.get(index).getFee());
+				feeList.get(index).setUserid(j);
+			}
+		}else{
+			if(a>=0){
+				Fee fee=new Fee(j,a);
+				feeList.add(fee);
+			}else{
+				Fee fee=new Fee(j,0);
+				feeList.add(fee);
+			}
+		}
+
+	}
+    
+}
