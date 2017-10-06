48cb193e5e8b5c9d4bd2aa264b4fb154027a6f5b
Cole Dorma <coledorma@hotmail.com>
2017-10-06 10:26:45 -0400

    Test and implement LoanTable class and getLoanTable method as TitleTable needs the LoanTable object.

---
+++ src/com/tests/LoanTableTests.java
@@ -0,0 +1,36 @@
+package com.tests;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.logic.model.Loan;
+import server.logic.tables.LoanTable;
+
+public class LoanTableTests {
+	LoanTable loanTable = null;
+	
+	@Before
+	public void setup() {
+		loanTable = loanTable.getInstance();
+	}
+	
+	@Test
+	public void testGetLoanTable() {	
+		List<Loan> loanList = loanTable.getLoanTable();
+		
+		assertEquals(1, loanList.get(1).getUserid());
+		assertEquals("1000098", loanList.get(1).getIsbn());
+		assertEquals("2", loanList.get(1).getCopynumber());
+		assertEquals("1", loanList.get(1).getRenewstate());
+		assertEquals("77777", loanList.get(1).getLoanID());
+		
+		assertEquals("88888", loanList.get(2).getLoanID());
+		
+		assertEquals("9781442668584", loanList.get(0).getIsbn());
+	}
+	
+}
---
+++ src/server/logic/tables/LoanTable.java
@@ -0,0 +1,38 @@
+package server.logic.tables;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import server.logic.model.Loan;
+
+public class LoanTable {
+	List<Loan> loanList=new ArrayList<Loan>();
+	
+    private static class LoanListHolder {
+        private static final LoanTable INSTANCE = new LoanTable();
+    }
+    
+    private LoanTable(){
+    	//set up the default list with some instances
+    	Loan loan = new Loan(0,"9781442668584","1",new Date(),"0", "10009");
+    	Loan loan2 = new Loan(1,"1000098","2",new Date(),"1", "77777");
+    	Loan loan3 = new Loan(2,"1000777","3",new Date(),"1", "88888");
+    	loanList.add(loan);
+    	loanList.add(loan2);
+    	loanList.add(loan3);
+    };
+    
+    public static final LoanTable getInstance() {
+        return LoanListHolder.INSTANCE;
+    }
+    
+    public List<Loan> getLoanTable() {
+		return loanList;
+	}
+    
+}
