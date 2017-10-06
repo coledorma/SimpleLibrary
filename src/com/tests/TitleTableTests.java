db97e6da333fb7f9d7568ca9dc0128e72b38f3ab
Cole Dorma <coledorma@hotmail.com>
2017-10-06 09:38:27 -0400

    Test getTitleTable and implement TitleTable as ItemTable will need to use the TitleTable object

---
+++ src/com/tests/TitleTableTests.java
@@ -0,0 +1,34 @@
+package com.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.logic.model.Title;
+import server.logic.tables.TitleTable;
+
+public class TitleTableTests {
+	TitleTable titleTable = null;
+	
+	@Before
+	public void setup() {
+		titleTable = TitleTable.getInstance();
+		
+	}
+	
+	@Test
+	public void testGetTitleTable() {
+		List<Title> titleList = titleTable.getTitleTable();
+		String[] ISBNList=new String[]{"9781442668584","9781442616899","9781442667181","9781611687910","9781317594277"};
+		String[] titlenameList=new String[]{"By the grace of God","Dante's lyric poetry ","Courtesy lost","Writing for justice","The act in context"};
+		for (int i = 0; i < ISBNList.length; i++) {
+			assertEquals(ISBNList[i], titleList.get(i).getISBN());
+			assertEquals(titlenameList[i], titleList.get(i).getBooktitle());
+		}
+	}
+
+}
+
