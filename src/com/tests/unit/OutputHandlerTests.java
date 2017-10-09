7ccba6db9c623b49757a2b972323d3cdb56a680b
Cole Dorma <coledorma@hotmail.com>
2017-10-09 13:36:37 -0400

    Tests and implementation for createTitle in OutputHandler class. Also added more testing and implementation for createUser method in OutputHandler class.

--- src/com/tests/unit/OutputHandlerTests.java
+++ src/com/tests/unit/OutputHandlerTests.java
@@ -2,19 +2,29 @@ package com.tests.unit;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import server.logic.handler.OutputHandler;
 import server.logic.handler.model.Output;
+import server.logic.model.Title;
+import server.logic.model.User;
+import server.logic.tables.TitleTable;
+import server.logic.tables.UserTable;
 
 public class OutputHandlerTests {
 	
 	OutputHandler outH = null;
+	UserTable userTable = null;
+	TitleTable titleTable = null;
 	
 	@Before
 	public void setup() {
 		outH =  new OutputHandler();
+		userTable = UserTable.getInstance();
+		titleTable = TitleTable.getInstance();
 	}
 	
 	@Test
@@ -24,13 +34,16 @@ public class OutputHandlerTests {
 		assertEquals(out.getState(), outH.createUser("hey").getState());
 		
 		Output out2 = new Output("Success!",2);
-		assertEquals(out2.getOutput(), outH.createUser("me@carleton.ca,me").getOutput());
-		assertEquals(out2.getState(), outH.createUser("me@carleton.ca,me").getState());
+		Output create = outH.createUser("cole@carleton.ca,me");
+		assertEquals(out2.getOutput(), create.getOutput());
+		assertEquals(out2.getState(), create.getState());
 		
 		Output out3 = new Output("The User Already Exists!",2);
 		assertEquals(out3.getOutput(), outH.createUser("Zhibo@carleton.ca,zhibo").getOutput());
 		assertEquals(out3.getState(), outH.createUser("Zhibo@carleton.ca,zhibo").getState());
 		
+		List<User> users = userTable.getUserTable();
+		assertEquals("cole@carleton.ca", users.get(users.size()-1).getUsername());
 	}
 	
 	@Test
@@ -54,4 +67,25 @@ public class OutputHandlerTests {
 	public void failIsNumber() {
 		assertEquals(false, OutputHandler.isNumber("hey"));
 	}
+	
+	@Test
+	public void testCreateTitle() {
+		Output out = new Output("Your input should in this format:'ISBN,title',ISBN should be a 13-digit number",5);
+		assertEquals(out.getOutput(), outH.createTitle("hey").getOutput());
+		assertEquals(out.getState(), outH.createTitle("hey").getState());
+		
+		Output out2 = new Output("Success!",2);
+		Output create = outH.createTitle("7777777777777,coledorma");
+		assertEquals(out2.getOutput(), create.getOutput());
+		assertEquals(out2.getState(), create.getState());
+		
+		Output out3 = new Output("The Title Already Exists!",2);
+		assertEquals(out3.getOutput(), outH.createTitle("9781442668584,By the grace of God").getOutput());
+		assertEquals(out3.getState(), outH.createTitle("9781442668584,By the grace of God").getState());
+		
+		List<Title> titles = titleTable.getTitleTable();
+		assertEquals("coledorma", titles.get(titles.size()-1).getBooktitle());
+	}
+	
+	
 }
