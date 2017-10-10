f2ddb5ffb752203c81016932579f5085eae564a5
Cole Dorma <coledorma@hotmail.com>
2017-10-10 18:57:09 -0400

    Updated Trace class so that the project runs at the school computers properly.

--- src/utilities/Trace.java
+++ src/utilities/Trace.java
@@ -14,7 +14,7 @@ public class Trace {
 	
 	private Trace() {
 		String userDir = System.getProperty("user.dir");
-		String configFile = String.format("properties/log4j.properties");
+		String configFile = String.format("%s//%s//%s.properties",userDir, "properties","log4j");
 		PropertyConfigurator.configure(configFile);
 	}
 
